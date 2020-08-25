966e545365dc3809f4f053b991c2a267e7f50d72
Martins <martins@zabarovski.lv>
2020-08-25 19:24:21 +0300

    Poga ar counteri + toast

--- app/src/main/java/lv/zabarovski/m/rtu_sample1/MainActivity.kt
+++ app/src/main/java/lv/zabarovski/m/rtu_sample1/MainActivity.kt
@@ -2,10 +2,19 @@ package lv.zabarovski.m.rtu_sample1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var clicks = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainCounter.text = "$clicks"
+        mainCountButton.setOnClickListener {
+            Toast.makeText(this, "Added one click", Toast.LENGTH_LONG).show()
+            clicks = clicks + 1
+            mainCounter.text = "$clicks"
+        }
     }
 }
